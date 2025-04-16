7257643eac3190d4a76b1e86428f9a860234d3c1
Ashish Lavale <ashishblavale5@gmail.com>
2025-04-16 19:27:58 +0530

    Roboshop E to E Test

---
+++ rabbitmq.sh
@@ -0,0 +1,10 @@
+cp rabbitmq.service /etc/systemd/system/rabbitmq.repo
+
+dnf install rabbitmq-server -y
+
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
