c95b09131479e83d8cce523ba5b145b485f603c9
Ashish Lavale <ashishblavale5@gmail.com>
2025-04-17 12:24:24 +0530

    Roboshop E to E Test

--- catalogue.sh
+++ catalogue.sh
@@ -4,11 +4,13 @@ dnf install nodejs -y
 
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
+
 cd /app
 npm install
 
