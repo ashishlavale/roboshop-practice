70334b2ae39016496b2f335d090e225969019660
Ashish Lavale <ashishblavale5@gmail.com>
2025-04-20 15:31:43 +0530

    Function Practice

--- catalogue.sh
+++ catalogue.sh
@@ -1,23 +1,13 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+component_name=catalogue
+source common.sh
+pwd=${pwd}
 
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+nodejs
 
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-
-cd /app
-npm install
-
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+cp ${pwd}/mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-mongosh -y
-
 mongosh --host mongodb-dev.ashishlavale5.online </app/db/master-data.js
+
+systemd_setup
+
