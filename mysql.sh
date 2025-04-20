61c9c5b4f837925719db1644c6c11c47af685a25
Ashish Lavale <ashishblavale5@gmail.com>
2025-04-20 18:09:43 +0530

    Condition

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,17 @@
-dnf install mysql-server -y
+if [ -z "$MYSQL_ROOT_PASSWORD" ] && [ -z "$1" ]; then
+  echo "input password is missing"
+  exit 1
+fi
 
+if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
+  PASSWORD="$MYSQL_ROOT_PASSWORD"
+fi
+
+if [ -n "$1" ]; then
+  PASSWORD="$1"
+fi
+
+dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
-
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass $PASSWORD
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,10 +1,14 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+if [ -z "$1" ]; then
+  echo "input password is missing"
+  exit 1
+fi
 
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y
 
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop $1
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
