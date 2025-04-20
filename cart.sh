963347b637faff01c6800be5d9cee0eb30ae9d04
Ashish Lavale <ashishblavale5@gmail.com>
2025-04-20 15:00:12 +0530

    Function Practice

--- cart.sh
+++ cart.sh
@@ -1,18 +1,7 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+component_name=cart
+source from common.sh
 
-cp cart.service /etc/systemd/system/cart.service
+nodejs
 
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
-unzip /tmp/cart.zip
+systemd_setup
 
-cd /app
-npm install
-
-systemctl daemon-reload
-systemctl enable cart
-systemctl start cart
---
+++ common.sh
@@ -0,0 +1,24 @@
+pwd=${pwd}
+
+systemd_setup() {
+  cp $(pwd)/${comonent_name} /etc/systemd/system/${comonent_name}
+  systemctl daemon-reload
+  systemctl enable ${comonent_name}
+  systemctl start cart ${comonent_name}
+}
+
+app_pre_setup() {
+  useradd roboshop
+  mkdir /app
+  curl -L -o /tmp/${comonent_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${comonent_name}-v3.zip
+  cd /app
+  unzip /tmp/${comonent_name}.zip
+}
+
+nodejs() {
+  dnf module disable nodejs -y
+  dnf module enable nodejs:20 -y
+  dnf install nodejs -y
+  app_pre_setup
+  npm install
+}
